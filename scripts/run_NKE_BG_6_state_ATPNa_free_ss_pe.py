9ab9ca8b13a6879665c1eb7cdc69f36a9f484571
Weiwei Ai <wai484@aucklanduni.ac.nz>
2025-09-29 11:18:05 +1300

    Add scripts to run pe

---
+++ scripts/run_NKE_BG_6_state_ATPNa_free_ss_pe.py
@@ -0,0 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pathlib import Path
+from run_sedmls import run_simulation
+
+parent_path = Path(__file__).parent.parent / 'cad' / 'models'
+simulation_path = Path(__file__).parent.parent / 'cad' / 'models' / 'simulation'
+
+full_path= parent_path / 'NKE_BG_6_state_ATPNa_free_fixedV_ss_pe.sedml'
+ss_time={'fit_1':0.3,'fit_2':0.3,'fit_3':0.3,'fit_4':0.3}
+run_simulation(full_path,rel_out_path='./simulation', external_variables_info={},
+                  external_variables_values=[],ss_time=ss_time)
