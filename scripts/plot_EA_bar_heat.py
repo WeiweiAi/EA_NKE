b911cea2992c66486eed1ea1c8e5001618ef56d8
Weiwei Ai <wai484@aucklanduni.ac.nz>
2026-01-09 14:39:45 +1300

    Read the heat production numbers from the files

--- scripts/plot_EA_bar_heat.py
+++ scripts/plot_EA_bar_heat.py
@@ -18,21 +18,14 @@ from matplotlib.ticker import LinearLocator
 import pandas as pd
 simulation_path = Path(__file__).parent.parent / 'cad' / 'models' / 'simulation'
 
-heat=[
-8.883614056,
-919.5973243,
-0.034704217,
-0.078252618,
-6521.711372,
-1.511295273,
-
-]
+heat_6_file=Path(simulation_path / 'report_task_NKE_BG_6_state_ATPNaZK_free_default_T1000ms_0EA_summary.csv').as_posix()
+heat_6state=pd.read_csv(heat_6_file)['meanPower'].values[0:6]
 labels=['$Rx_m^1$','$Rx_m^2$','$Rx_m^3$','$Rx_m^4$','$Rx_m^5$','$Rx_m^6$']
 
 # plot bar chart
 fig, ax = plt.subplots(figsize=(3,3))
 y_pos = np.arange(len(labels))
-plt.bar(y_pos, np.array(heat)/1000, align='center', alpha=0.5)
+plt.bar(y_pos, np.array(heat_6state)/1000, align='center', alpha=0.5)
 plt.xticks(y_pos, labels)
 plt.xlabel('Reactions')
 plt.ylabel('Heat production rate (pW)')
@@ -42,23 +35,9 @@ ax.yaxis.grid(True, linestyle='--', alpha=0.5)
 fig.savefig(Path(simulation_path / 'EA_6state_heat_bar.png').as_posix(), dpi=300)
 plt.show()
 
-heat_15state=[
-    0.377132003,
-0.006511733,
-0.094183146,
-0.026371207,
-17.1873785,
-40344.98808,
-339.2060174,
-511.6383602,
-12.12724678,
-0.319967199,
-9.9295053,
-1.169767399,
-28073.21989,
-1.143204242,
-6438.534285,
-]
+heat_15_file=Path(simulation_path / 'report_task_NKE_BG_15_state_default_T1000ms_0EA_summary.csv').as_posix()
+heat_15state=pd.read_csv(heat_15_file)['meanPower'].values[0:15]
+
 labels_15state=['$Rx_m^1$','$Rx_m^2$','$Rx_m^3$','$Rx_m^4$','$Rx_m^5$','$Rx_m^6$','$Rx_m^7$','$Rx_m^8$','$Rx_m^9$','$Rx_m^{10}$','$Rx_m^{11}$','$Rx_m^{12}$','$Rx_m^{13}$','$Rx_m^{14}$','$Rx_m^{15}$']
 # plot bar chart
 fig, ax = plt.subplots(figsize=(6,3))
