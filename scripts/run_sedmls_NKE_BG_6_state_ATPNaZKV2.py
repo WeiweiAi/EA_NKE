8546b30fd75324b746b3b6482c7ef422d4cbad4f
Weiwei Ai <wai484@aucklanduni.ac.nz>
2026-01-14 13:47:06 +1300

    Add scripts to run simulation and pe task for V2

---
+++ scripts/run_sedmls_NKE_BG_6_state_ATPNaZKV2.py
@@ -0,0 +1,9 @@
+from run_sedmls import run_sim_jsons
+
+sedml_jsonfiles=['NKE_BG_6_state_ATPNaZKV2_SS_fixedV_sedmls.json','NKE_BG_6_state_ATPNaZKV2_sedmls.json',
+                 'NKE_BG_6_state_ATPNaZKV2_SS_sedmls.json','NKE_BG_6_state_ATPNaZKV2_fixedV_sedmls.json'
+                ]
+
+run_sim_jsons (sedml_jsonfiles)
+
+   
